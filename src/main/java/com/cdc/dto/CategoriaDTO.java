3b06db64e4fbceb9411cf0f4da9d6a53e845a1b2
faellarujo <raphaelaraujo.ti@gmail.com>
2024-04-22 19:19:53 +0100

    "Ajustes no Create Request"

--- src/main/java/com/cdc/dto/CategoriaDTO.java
+++
@@ -1,8 +0,0 @@
-package com.cdc.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-
-public record CategoriaDTO(@NotBlank String nome) {
-
-}
--- src/main/java/com/cdc/repository/NomeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cdc.repository;
-
-import org.springframework.data.jpa.repository.Query;
-
-public interface NomeRepository<T> {    public T findByNome(String nome); //1
-
-
-}
